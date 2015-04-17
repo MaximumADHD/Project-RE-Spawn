98009f7932737c8e81b43004cf2572b3953db138
Max <1701125@YHS-C128C30.y115.org>
2015-04-17 07:22:59 -0500

    Added Level Startup Screen

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelData : MonoBehaviour 
+{
+    public string levelName = "LevelName";
+    public AudioClip music;
+    public GUIStyle levelScreenModifier;
+    private int framesPassed = 0;
+    private float opacity = 1;
+
+    public void OnGUI()
+    {
+        framesPassed++;
+        if (framesPassed > 200)
+        {
+            opacity = opacity - 0.01f;
+        }
+        GUI.color = new Color(1, 1, 1, opacity);
+        if (framesPassed < 360)
+        {
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), levelName, levelScreenModifier);
+        }
+    }
+}
