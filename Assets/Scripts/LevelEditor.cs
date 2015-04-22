ce1b4a048d6de22529e996eb89e62eefb56b8b9f
Max <1701125@YHS-C128C30.y115.org>
2015-04-22 10:33:11 -0500

    Finishing up the level editor

--- Assets/Scripts/LevelEditor.cs
+++ Assets/Scripts/LevelEditor.cs
@@ -1,48 +1,94 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
 
 public class LevelEditor : MonoBehaviour 
 {
     public GameObject[] objects;
+    public GameObject highlighter;
     public Camera myCamera;
     private int currentSprite = 0;
-    private bool guiMouseDown = false;
-    private bool mouseDown = false;
-    private string mode = "Create";
+    private bool levelOpen = false;
+    private bool windowOpen = false;
+    private bool inputtingNew = false;
+    private string levelName = "Level1";
+    private string currentPath;
+    private float zoom = -10;
+    private SpriteRenderer sprite;
+    private string mode = "None";
+    private int flipDebounce = 0;
 
     public void OnGUI()
     {
-        GUI.Label(new Rect(5, 3, 100, 30), "GameObject:");
-        GUI.Label(new Rect(5, 60, 100, 35), "Mode:");
-        bool isPressingItemSelect = GUI.Button(new Rect(5, 20, 100, 35), objects[currentSprite].name);
-        bool isPressingMode = GUI.Button(new Rect(5, 80, 100, 35), mode);
-        if (!Input.GetMouseButtonDown(0) && guiMouseDown)
+        if (levelOpen)
         {
-            guiMouseDown = false;
+            GUI.Label(new Rect(5, 3, 100, 30), "GameObject:");
+            GUI.Label(new Rect(5, 60, 100, 35), "Mode:");
+            GUI.Label(new Rect(5, 120, 150, 500), "Editor Controls:\n\n(WASD/Arrow Keys)\nMove Camera\n\n(Left Shift)\nFast Camera\n\n(MouseWheel Up/Down)\nZoom Camera\n\n(Left Mouse)\nPerform an action based on the current mode.\n\n(Q)\nChange Block\n\n(E)\nChange Mode");
+            GUI.Button(new Rect(5, 20, 150, 35), objects[currentSprite].name);
+            GUI.Button(new Rect(5, 80, 150, 35), mode);
         }
-        if (isPressingItemSelect && !guiMouseDown)
+        else
         {
-            guiMouseDown = true;
-            currentSprite = (currentSprite + 1) % objects.Length;
-        }
-        else if (isPressingMode && !guiMouseDown)
-        {
-            guiMouseDown = true;
-            if (mode.Equals("Create"))
+            if (inputtingNew)
             {
-                mode = "Destroy";
+                GUI.Label(new Rect(5, 3, 200, 30), "Enter Level Name:");
+                levelName = GUI.TextField(new Rect(5, 23, 100, 20), levelName);
+                bool isPressing = GUI.Button(new Rect(5, 46, 100, 20), "Create");
+                if (isPressing)
+                {
+                    string path = EditorUtility.SaveFolderPanel("Choose a Folder to save in","","");
+                    if (path.Length > 0)
+                    {
+                        currentPath = path;
+                        levelOpen = true;
+                    }
+                }
             }
             else
             {
-                mode = "Create";
+                bool pressingNew = GUI.Button(new Rect(5, 3, 100, 30), "New Level");
+                bool pressingOpen = GUI.Button(new Rect(5, 33, 100, 30), "Open Level");
+                if (pressingNew)
+                {
+                    inputtingNew = true;
+                }
             }
+
         }
     }
 
-    public void Start()
+    public void InputUpdate()
     {
-        myCamera.transform.localPosition = new Vector3(0, 0, -10);
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            currentSprite = (currentSprite + 1) % objects.Length;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (mode.Equals("None"))
+            {
+                mode = "Create";
+                sprite.color = new Color(0, 1, 0, 1);
+            }
+            else if (mode.Equals("Create"))
+            {
+                mode = "Destroy";
+                sprite.color = new Color(1, 0, 0, 1);
+            }
+            else if (mode.Equals("Destroy"))
+            {
+                mode = "Flip";
+                flipDebounce = 10;
+                sprite.color = new Color(1, 1, 0, 1);
+            }
+            else if (mode.Equals("Flip"))
+            {
+                mode = "None";
+                sprite.color = new Color(1, 1, 1, 0.25f);
+            }
+        }
     }
 
     public void CameraUpdate()
@@ -70,19 +116,21 @@ public class LevelEditor : MonoBehaviour
         {
             x = -speed;
         }
-        myCamera.transform.Translate(new Vector3(x, y, 0));
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        zoom = Mathf.Min(-3,Mathf.Max(-15,zoom + (scroll*3)));
+        Vector3 cur = myCamera.transform.localPosition;
+        myCamera.transform.localPosition = new Vector3(cur.x + x, cur.y + y, zoom);
     }
 
-    public List<GameObject> getMouseHits()
+    public List<GameObject> GetMouseHits()
     {
         List<GameObject> hit = new List<GameObject>();
-        Ray mouseOut = myCamera.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(mouseOut.origin,mouseOut.direction,Color.red);
-        Vector3 pos = new Vector3(Mathf.Ceil(mouseOut.origin.x), Mathf.Ceil(mouseOut.origin.y));
+        Ray mouseOut = myCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        Vector3 pos = new Vector3(Mathf.Ceil(mouseOut.origin.x-0.5f), Mathf.Ceil(mouseOut.origin.y-0.5f));
         GameObject[] objs = GameObject.FindObjectsOfType<GameObject>();
         foreach (GameObject obj in objs)
         {
-            if (obj.transform.localPosition == pos)
+            if (obj.transform.localPosition == pos && !obj.tag.Equals("LevelEdit"))
             {
                 hit.Add(obj);
             }
@@ -90,14 +138,14 @@ public class LevelEditor : MonoBehaviour
         return hit;
     }
 
-    public void Update()
+    public void ModeUpdate()
     {
-        CameraUpdate();
-        if (Input.GetKey(KeyCode.Mouse0) && !guiMouseDown)
+        Ray mouseOut = myCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        Vector3 pos = new Vector3(Mathf.Ceil(mouseOut.origin.x-0.5f), Mathf.Ceil(mouseOut.origin.y-0.5f));
+        highlighter.transform.localPosition = pos;
+        if (Input.GetKey(KeyCode.Mouse0))
         {
-            Ray mouseOut = myCamera.ScreenPointToRay(Input.mousePosition);
-            Vector3 pos = new Vector3(Mathf.Ceil(mouseOut.origin.x), Mathf.Ceil(mouseOut.origin.y));
-            List<GameObject> hit = getMouseHits();
+            List<GameObject> hit = GetMouseHits();
             if (mode.Equals("Create"))
             {
                 if (hit.Count == 0)
@@ -113,6 +161,42 @@ public class LevelEditor : MonoBehaviour
                     Object.DestroyImmediate(obj);
                 }
             }
+            else if (mode.Equals("Flip"))
+            {
+                flipDebounce++;
+                Debug.Log(flipDebounce);
+                if (flipDebounce >= 10)
+                {
+                    flipDebounce = 0;
+                    foreach (GameObject obj in hit)
+                    {
+                        Vector3 cur = obj.transform.localScale;
+                        obj.transform.localScale = new Vector3(-cur.x, cur.y, cur.z);
+                    }
+                }
+
+            }
+        }
+    }
+
+    public void Start()
+    {
+        sprite = highlighter.GetComponent<SpriteRenderer>();
+        sprite.color = new Color(1, 1, 1, 0.25f);
+        myCamera.transform.localPosition = new Vector3(0, 0, -10);
+    }
+
+    public void Update()
+    {
+        if (levelOpen)
+        {
+            CameraUpdate();
+            InputUpdate();
+            ModeUpdate();
+        }
+        else
+        {
+            sprite.color = new Color(0, 0, 0, 0);
         }
     }
 
